b38c7a34847f4d019d2fdcb1ebe037620ce5d3d9
Evoreek <Evoreek@outlook.com>
2024-05-31 12:03:00 +0800

    提交

--- Runtime/Core/NodeGraphProcessor/Examples/Scenes/RuntimeGraph/RuntimeGraph.cs
+++ Runtime/Core/NodeGraphProcessor/Examples/Scenes/RuntimeGraph/RuntimeGraph.cs
@@ -25,7 +25,6 @@ public class RuntimeGraph : MonoBehaviour
 			graph.SetParameterValue("Input", (float)i++);
 			graph.SetParameterValue("GameObject", assignedGameObject);
 			processor.Run();
-			Debug.Log("Output: " + graph.GetParameterValue("Output"));
 		}
     }
 }
