b92894b6ae84e446f1b75ebb21ce73ab1f2e3ce4
Evoreek <wh_961230@163.com>
2024-06-22 18:11:04 +0800

    提交

--- Editor/LazyPanGuide.cs
+++ Editor/LazyPanGuide.cs
@@ -62,8 +62,6 @@ namespace LazyPan {
                     CopyFilesToDirectory("Bundles/Scenes", "LazyPan/Bundles/Scenes");
                     CopyFilesToDirectory("Bundles/TextMeshPro", "");
                     CopyFilesToDirectory("Bundles/Csv/StreamingAssets/Csv", "StreamingAssets/Csv");
-                    DecompressFileToDirectory("Core/NodeGraphProcessor.zip",
-                        "LazyPan/Scripts/GamePlay/Config/NodeGraphProcessor");
                 }
 
                 GUILayout.EndHorizontal();
@@ -339,12 +337,6 @@ namespace LazyPan {
             Debug.Log($"Files copied from {sourcePath} to {targetPath}");
 
             AssetDatabase.Refresh();
-        }
-
-        private void DecompressFileToDirectory(string sourceZipFile, string destinationFolder) {
-            string combine_sourceZipFile = $"Packages/evoreek.lazypan/Runtime/{sourceZipFile}"; // 源文件夹路径
-            string combine_destinationFolder = $"Assets/{destinationFolder}"; // 目标文件夹路径;
-            LazyPanTool.DecompressFile(combine_sourceZipFile, combine_destinationFolder);
         }
 
         private void CopyDirectory(string sourceDir, string destDir) {
