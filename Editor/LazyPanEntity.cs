60154a8eea9c3e10da54202eae841ad6ccd1d09a
Evoreek <wh_961230@163.com>
2024-06-27 17:45:56 +0800

    提交

--- Editor/LazyPanEntity.cs
+++ Editor/LazyPanEntity.cs
@@ -1,13 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 namespace LazyPan {
     public class LazyPanEntity : EditorWindow {
+        private string _instanceFlowName;
+        private string _instanceTypeName;
         private string _instanceObjName;
+        private string _instanceObjChineseName;
         public void OnCustomGUI(float areaX) {
             GUILayout.BeginArea(new Rect(areaX, 30, Screen.width, Screen.height));
 
@@ -23,13 +26,27 @@ namespace LazyPan {
             }
             GUILayout.EndHorizontal();
 
+            GUILayout.BeginVertical();
             GUILayout.BeginHorizontal();
-            _instanceObjName = EditorGUILayout.TextField(_instanceObjName, GUILayout.Height(50));
+            _instanceFlowName = EditorGUILayout.TextField("流程标识(必填)", _instanceFlowName, GUILayout.Height(20));
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            _instanceTypeName = EditorGUILayout.TextField("类型标识(必填)", _instanceTypeName, GUILayout.Height(20));
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            _instanceObjName = EditorGUILayout.TextField("实体标识(必填)", _instanceObjName, GUILayout.Height(20));
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            _instanceObjChineseName = EditorGUILayout.TextField("实体中文名字(必填)", _instanceObjChineseName, GUILayout.Height(20));
+            GUILayout.EndHorizontal();
             style = LazyPanTool.GetGUISkin("AButtonGUISkin").GetStyle("button");
-            if(GUILayout.Button("创建实体物体并跳转到目录下", style, GUILayout.Height(80))) {
+            if(GUILayout.Button("创建实体物体", style, GUILayout.Height(80))) {
                 InstanceCustomObj();
             }
-            GUILayout.EndHorizontal();
+            if(GUILayout.Button("创建实体物体点位配置", style, GUILayout.Height(80))) {
+                InstanceCustomLocationSetting();
+            }
+            GUILayout.EndVertical();
         
             GUILayout.EndArea();
         }
@@ -40,6 +57,9 @@ namespace LazyPan {
         }
 
         private void InstanceCustomObj() {
+            if (_instanceObjName == "" || _instanceTypeName == "" || _instanceFlowName == "" || _instanceObjChineseName == "") {
+                return;
+            }
             string sourcePath = "Packages/evoreek.lazypan/Runtime/Bundles/Prefabs/Obj/Obj_Sample_Sample.prefab"; // 替换为你的预制体源文件路径
             string targetFolderPath = "Assets/LazyPan/Bundles/Prefabs/Obj"; // 替换为你想要拷贝到的目标文件夹路径
             
@@ -62,9 +82,39 @@ namespace LazyPan {
                 AssetDatabase.Refresh();
                 
                 //修改资源的名字为自定义
-                AssetDatabase.RenameAsset(targetPath, _instanceObjName);
+                AssetDatabase.RenameAsset(targetPath,
+                    string.Concat(_instanceFlowName, _instanceFlowName != null ? "/" : "", "Obj_", _instanceTypeName,
+                        "_", _instanceObjName));
                 AssetDatabase.Refresh();
             }
+        }
+
+        private void InstanceCustomLocationSetting() {
+            if (_instanceObjName == "" || _instanceTypeName == "" || _instanceFlowName == "" || _instanceObjChineseName == "") {
+                return;
+            }
+
+            // 创建实例并赋值
+            LocationInformationSetting testAsset = CreateInstance<LocationInformationSetting>();
+            testAsset.SettingName = _instanceObjChineseName;
+            testAsset.locationInformationDatas = new List<LocationInformationData>();
+            testAsset.locationInformationDatas.Add(new LocationInformationData());
+
+            // 替换为你希望保存的目录路径，例如 "Assets/MyFolder/"
+            string savePath = "Assets/LazyPan/Bundles/Configs/Setting/LocationInformationSetting/";
+
+            // 确保目标文件夹存在，如果不存在则创建
+            if (!AssetDatabase.IsValidFolder(savePath)) {
+                AssetDatabase.CreateFolder("Assets", "LazyPan/Bundles/Configs/Setting/LocationInformationSetting");
+            }
+
+            // 生成一个唯一的文件名
+            string assetPath = AssetDatabase.GenerateUniqueAssetPath(savePath + string.Concat("LocationInf_", _instanceFlowName, "_", _instanceObjName, ".asset"));
+
+            // 将实例保存为.asset文件
+            AssetDatabase.CreateAsset(testAsset, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
     }
 }
