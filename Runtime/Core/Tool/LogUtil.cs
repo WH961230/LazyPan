d31e21a34f89810e81f8a647c4c9359432ca5653
Shovelhoney <60457275+WH961230@users.noreply.github.com>
2024-07-19 10:47:00 +0800

    Update LogUtil.cs

--- Runtime/Core/Tool/LogUtil.cs
+++ Runtime/Core/Tool/LogUtil.cs
@@ -1,21 +1,17 @@
 ﻿namespace LazyPan {
     public static class LogUtil {
-        [System.Diagnostics.Conditional("ENABLE_LOG")]
         public static void Log(object message) {
             UnityEngine.Debug.Log(message);
         }
         
-        [System.Diagnostics.Conditional("ENABLE_LOG")]
         public static void LogError(object message) {
             UnityEngine.Debug.LogError(message);
         }
 
-        [System.Diagnostics.Conditional("ENABLE_LOG")]
         public static void LogFormat(string message, params object[] param) {
             UnityEngine.Debug.LogFormat(message, param);
         }
         
-        [System.Diagnostics.Conditional("ENABLE_LOG")]
         public static void LogErrorFormat(string message, params object[] param) {
             UnityEngine.Debug.LogErrorFormat(message, param);
         }
